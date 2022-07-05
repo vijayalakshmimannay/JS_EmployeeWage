57aa5a09e8341426ea4837560036f910f45d382b
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-07-05 10:39:25 +0530

    [vijayalakshmi] check employee hours is part timeor full time using switch

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,12 +1,29 @@
-//UC1 IF Else condition
-{
-    const IS_ABSENT = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 2;
-    if(empCheck == IS_ABSENT)
+//UC2 Switch Case
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PERHOUR = 20;
+
+let empHrs = 0;
+let empCheck  = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck )
     {
-        console.log("Employee is Absent");
-        return;
-    } else {
-    console.log("Employee is Present");
-}
-}
+        case IS_PART_TIME:
+        
+            empHrs = PART_TIME_HOURS;
+            break;
+        
+        case IS_FULL_TIME:
+        
+            empHrs = FULL_TIME_HOURS;
+            break;
+            
+        default:
+            empHrs = 0;    
+     }
+
+     let empWage = empHrs * WAGE_PERHOUR;
+     console.log("EmpWage: " + empWage);
+ 
+    
