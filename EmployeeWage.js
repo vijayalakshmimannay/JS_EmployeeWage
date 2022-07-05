e1cef5adba5290cef0adda1fef5204fc44e4610d
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-07-05 10:51:28 +0530

    [vijayalakshmi] check employee hours is part timeor full time using Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,27 +3,25 @@ const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
-const WAGE_PERHOUR = 20;
+const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-let empCheck  = Math.floor(Math.random() * 10) % 3;
+function getWorkingHours(empCheck) {
     switch(empCheck )
     {
         case IS_PART_TIME:
         
-            empHrs = PART_TIME_HOURS;
-            break;
-        
+            return PART_TIME_HOURS;
+           
         case IS_FULL_TIME:
         
-            empHrs = FULL_TIME_HOURS;
-            break;
-            
+            return FULL_TIME_HOURS;
+                
         default:
-            empHrs = 0;    
+            return 0;    
      }
-
-     let empWage = empHrs * WAGE_PERHOUR;
-     console.log("EmpWage: " + empWage);
- 
-    
+}
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: " + empWage);
