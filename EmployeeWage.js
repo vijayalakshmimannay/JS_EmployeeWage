df69536d311008c9dbdbda1def6befc6a4f35def
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-07-05 13:56:27 +0530

    [vijayalakshmi] store the dailwage value along with total wage using array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,36 +3,38 @@ const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const No_Of_Working_Days = 20;
-//UC 3 Using Function
-function getWorkingHours(empCheck) {
-    //UC 2 Using Switch Case
-    switch(empCheck )
-    {
-        case IS_PART_TIME:
+const WAGE_PER_HOUR  = 20;
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+   {
+    case IS_PART_TIME:
         
-            return PART_TIME_HOURS;
-           
-        case IS_FULL_TIME:
+        return PART_TIME_HOURS;
+       
+    case IS_FULL_TIME:
+    
+        return FULL_TIME_HOURS;
+            
+    default:
+        return 0;    
         
-            return FULL_TIME_HOURS;
-                
-        default:
-            return 0;    
-     }
+   }
+} 
+function calDailyWage(empHrs) {
+   return empHrs * WAGE_PER_HOUR;
 }
-//UC5 calculate wages till a condtion of total working hours 160 or max days of 20 is reached for a month
-const MAX_HRS_IN_MONTH = 160;
-const NUM_OF_WORKING_DAYS= 20;
-let totalempHrs = 0;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160; 
+let totalEmpHrs = 0;
 let totalWorkingDays = 0;
-while (totalempHrs <= MAX_HRS_IN_MONTH &&
-    totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalempHrs += getWorkingHours(empCheck);
-
+let empDailyWageArr = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+   totalWorkingDays++;
+   let empCheck = Math.floor(Math.random() * 10) % 3;
+   let empHrs = getWorkingHours(empCheck);
+   totalEmpHrs += empHrs;
+   empDailyWageArr.push(calDailyWage(empHrs));
 }
-let empWage = totalempHrs * WAGE_PER_HOUR;
-console.log("Total Days : " + totalWorkingDays +  " , Total Working Hours : " + totalempHrs + " , Employee Wage : " + empWage);
+let empWages = calDailyWage(totalEmpHrs);
+console.log("Total Days: " + totalWorkingDays + "Total Hrs: " + totalEmpHrs + "Emp Wage: " + empWages);
